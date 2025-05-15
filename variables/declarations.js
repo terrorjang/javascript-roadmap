c2474718a69d9d5599ff6c25ebb5b2667643c109
youngseok.jang <ys123.jang@gmail.com>
2025-05-15 12:54:44 +0900

    declarations

---
+++ variables/declarations.js
@@ -0,0 +1,15 @@
+// A quick review of creating variables
+// Variations of using var
+var x = 10;
+var y = 20;
+var z = 30;
+
+var x = 10,
+  y = 20,
+  z = 30;
+console.log(x, y, z);
+
+// Rules and tips to naming variables
+// Checking for a variable's existence
+// Variable hoisting
+// The let keyword
